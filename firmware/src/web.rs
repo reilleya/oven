8a30ef9bad470f2225db6c39d1e1c0cbdfb43a22
Andrew Reilley <andrew.r.reilley@gmail.com>
2025-11-28 18:37:59 -0500

    Progress

--- firmware/src/web.rs
+++ firmware/src/web.rs
@@ -1,53 +1,52 @@
 use embassy_net::Stack;
 use embassy_time::Duration;
 use esp_alloc as _;
-use picoserve::{extract::State, AppBuilder, AppRouter, Router, response::File, routing};
 
 use picoserve::{
-    response::{with_state::WithStateUpdate, IntoResponse, IntoResponseWithState, Redirect},
+    AppBuilder, AppRouter, Router,
+    extract::State,
+    response::{File, IntoResponse, IntoResponseWithState, Redirect, with_state::WithStateUpdate},
+    routing,
     routing::{get, parse_path_segment},
 };
 
 use core::cell::RefCell;
 
-
-#[derive(serde::Deserialize)]
-struct SetupFormValue {
-    temperature: i32,
-    time: i32,
-}
-
 struct AppState {
     temperature: RefCell<i32>,
-    time: RefCell<i32>
+    time: RefCell<i32>,
 }
 
 #[derive(serde::Serialize)]
 struct AppStateValue {
     temperature: i32,
-    time: i32
+    time: i32,
 }
 
 impl picoserve::extract::FromRef<AppState> for AppStateValue {
-    fn from_ref(AppState { temperature, time, .. }: &AppState) -> Self {
+    fn from_ref(
+        AppState {
+            temperature, time, ..
+        }: &AppState,
+    ) -> Self {
         Self {
             temperature: *temperature.borrow(),
-            time: *time.borrow()
+            time: *time.borrow(),
         }
     }
 }
 
-async fn get_value(State(value): State<AppStateValue>) -> impl IntoResponse {
+async fn get_temperature(State(value): State<AppStateValue>) -> impl IntoResponse {
     picoserve::response::Json(value)
 }
 
-async fn increment_value() -> impl IntoResponseWithState<AppState> {
+async fn increment_temperature() -> impl IntoResponseWithState<AppState> {
     Redirect::to(".").with_state_update(async |state: &AppState| {
         *state.temperature.borrow_mut() += 1;
     })
 }
 
-async fn set_value(value: i32) -> impl IntoResponseWithState<AppState> {
+async fn set_temperature(value: i32) -> impl IntoResponseWithState<AppState> {
     Redirect::to("..").with_state_update(async move |state: &AppState| {
         *state.temperature.borrow_mut() = value;
     })
@@ -59,19 +58,27 @@ impl AppBuilder for Application {
     type PathRouter = impl routing::PathRouter;
 
     fn build_app(self) -> picoserve::Router<Self::PathRouter> {
-        let state = AppState { temperature: 0.into(), time: 0.into() };
+        let state = AppState {
+            temperature: 0.into(),
+            time: 0.into(),
+        };
 
         picoserve::Router::new()
             .route(
                 "/",
-                routing::get_service(File::html(include_str!("index.html"))),
-            ).route(
-                "/buttons.js",
-                routing::get_service(File::javascript(include_str!("buttons.js"))),
+                routing::get_service(File::html(include_str!("web/index.html"))),
             )
-            .route("/get", get(get_value))
-            .route("/increment", get(increment_value))
-            .route(("/set", parse_path_segment()), get(set_value))
+            .route(
+                "/buttons.js",
+                routing::get_service(File::javascript(include_str!("web/buttons.js"))),
+            )
+            .route(
+                "/styles.css",
+                routing::get_service(File::css(include_str!("web/styles.css"))),
+            )
+            .route("/get", get(get_temperature))
+            .route("/increment", get(increment_temperature))
+            .route(("/set", parse_path_segment()), get(set_temperature))
             .with_state(state)
     }
 }
